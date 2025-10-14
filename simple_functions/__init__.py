cf6688c8646ad9484165480c16d08cc4163c29d2
Dingyu Yang <dy225@ic.ac.uk>
2025-10-14 22:15:19 +0100

    CI exercise attempt 4

--- simple_functions/__init__.py
+++ simple_functions/__init__.py
@@ -1,5 +1,5 @@
 from .functions1 import *  # noqa
-
+from .constants import *
 try:
     from importlib.metadata import version, PackageNotFoundError  # Python 3.8+
 except ImportError:  # For Python < 3.8
